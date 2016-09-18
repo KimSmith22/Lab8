c68c4bf8b472fd991110e468cdaac4192a5e0271
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-18 15:40:01 -0400

    version works but does not compute batting average

--- src/BaseballAverages.java
+++ src/BaseballAverages.java
@@ -12,49 +12,56 @@ public class BaseballAverages {
 	public static void main(String[] args) {
 
 		// variables
-		int scoredAtBat= 0;
-		int slug = 0;
-		int batav = 0;
-		
-		
-		//sysout
+		String choice = "y";
+
+		// sysout
 		System.out.println("Batting Average Calculator");
 		System.out.println();
-	
-		//prompt for user input
-		System.out.println("Enter number of times at bat:  ");
-		Scanner k = new Scanner(System.in);
-		
-			//array of unknown length
-		int atBat = Integer.parseInt(k.nextLine());
-		int[] times = new int[atBat];
-		while(atBat > 4){//boolean statement to initiate loop & validation statement
-			System.out.println("Please enter a number between 0 and 4");
-			atBat = k.nextInt();
-		}//end loop
-		System.out.println();
-		
-		//scoring legend
-		System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
-		System.out.println();
-		
-		//code to print array data to console
-		int[] base = {0, 1, 0, 2, 3};
-		for(int i = 0; i < base.length; i++){
-			/*tried a bunch of different variables in the following line
-			 * in an attempt to see which variable would get the output
-			 * to increment; while i+1 works i am not getting expected result
+		do {// initate do while loop for continue statement
+			// prompt for user input
+			System.out.println("Enter number of times at bat:  ");
+			Scanner k = new Scanner(System.in);
+
+			// array of unknown length
+			int atBat = Integer.parseInt(k.nextLine());
+			int[] times = new int[atBat];
+			while (atBat > 5) {// boolean statement to initiate loop &
+								// validation statement
+				System.out.println("Please enter a number between 0 and 4");
+				atBat = k.nextInt();
+			} // end loop
+
+			// scoring legend
+			System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
+			System.out.println();
+
+			// code to print array data to console
+			int[] base = { 0, 4, 0, 1, 3 };
+			double sum = 0.000; // sets sum to double showing three places past
+								// decimal
+			for (int i = 0; i < times.length; i++) {// initiate for loop
+				sum += base[i];
+				/*
+				 * tried a bunch of different variables in the following line in
+				 * an attempt to see which variable would get the output to
+				 * increment;
+				 */
+				System.out.println("Result for at-bat " + (i) + ":  " + base[i]);
+			} // end for loop
+			System.out.println();
+			/*
+			 * batting average not computing correctly; need to divide by number
+			 * of instances where batter makes a base, currently dividing by
+			 * total number of times at bat.
 			 */
-			System.out.println("Result for at-bat " +(i+1) + ":  " + base[i]);
-		}
-			
-		/*call methods to invoke output
-		BaseballMethods.getBatAvg(atBat, scoredAtBat, base, batav);
-		BaseballMethods.getSlug(atBat, base, slug);
-		*/
-			
-		}//psvm
+			//System.out.println("Batting average is:  " + (sum / scored)); //scored is future variable to identify number of times scored at bat
+			System.out.println("Slugging Percent:  " + (sum/times.length));
+			System.out.println();
 
-	}// end class
+			System.out.println("Another batter?  y/n");
+			choice = k.nextLine();
+		} while (choice.equals("y"));
 
+	}// psvm
 
+}// end clas
