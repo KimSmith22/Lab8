0427f65df6461ecc2d007116872277c0528e43c3
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-17 20:43:52 -0400

    added loop

--- src/BaseballAverages.java
+++ src/BaseballAverages.java
@@ -21,7 +21,7 @@ public class BaseballAverages {
 		//sysout
 		System.out.println("Batting Average Calculator");
 		System.out.println();
-		
+	
 		//prompt for user input
 		System.out.println("Enter number of times at bat:  ");
 		Scanner k = new Scanner(System.in);
@@ -29,14 +29,14 @@ public class BaseballAverages {
 			//array of unknown length
 		int atBat = Integer.parseInt(k.nextLine());
 		int[] times = new int[atBat];
-			while (atBat > 4){//validation statement
+		if(atBat > 4){
 			System.out.println("Please enter a number between 0 and 4");
-			}//end validation
+		}else {
 		System.out.println();
 		
 		//scoring legend
 		System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
-		
+		}
 		//call methods to invoke output
 		BaseballMethods.getBatAvg(atBat, scoredAtBat, base, batav);
 		BaseballMethods.getSlug(atBat, base, slug);
