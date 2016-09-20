3c4a1342d1f2a428837c41c77c7a8d1386eae9cb
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-20 11:57:29 -0400

    some minor changes related to loops

--- src/BaseballAverages.java
+++ src/BaseballAverages.java
@@ -14,6 +14,7 @@ public class BaseballAverages {
 		// variables
 		int value = 0;
 		int[] array = null ;
+		int battters = 0;
 		String choice = "y";
 
 		// sysout
@@ -21,17 +22,22 @@ public class BaseballAverages {
 		System.out.println();
 		do {// initIate do while loop for continue statement
 			// prompt for user input
-			System.out.println("Enter number of times at bat:  ");
+			System.out.println("Please pick a player using 1 thru 5");
 			Scanner k = new Scanner(System.in);
+			int player = k.nextInt();
+			k.nextLine();
+			
+			System.out.println("Enter number of times at bat (1 thru 5):  ");
+			int atBat = k.nextInt();
 
 			// array of unknown length
-			int atBat = Integer.parseInt(k.nextLine());
 			int[] times = new int[atBat];
-			while (atBat < 0 || atBat > 4) {// boolean statement to initiate loop &
+			while (atBat < 1 || atBat > 5) {// boolean statement to initiate loop &
 								// validation statement
-				System.out.println("Please enter a number between 0 and 4")
-				;/*if the above line prints and user follows with a number
-				* within range an exception displays; not sure why
+				System.out.println("Please enter a number between 1 and 5");
+				/*
+				* line 30 requires coding both sides of the spectrum of numbers you want
+				* to be out of range for input within the program
 				*/
 				atBat = k.nextInt();
 			} // end loop
@@ -40,28 +46,30 @@ public class BaseballAverages {
 			System.out.println("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
 			System.out.println();
 
+		
 			/*
 			 * the following code will be replaced by more array data related to multiple players
 			 * a single array was used to test the program
 			 */
-			int[] base = { 0, 4, 0, 1, 3 }; // code to print array data to console
-			/*
-			 * int[]batters = new int[5];
-			 * batters[0] = { };
-			 * batters[1] = { };
-			 * batters[3] = { };
-			 * batters[4] = { };
-			 */
+			 int[] batters = {0, 4, 1, 3, 2}; // code to print array data to console; i need a multi-dimensional array here
+		
+//			 batters[0] = 3,  2, 1, 0, 0;
+//			 batters[1] = 2, 4, 4, 0, 1;
+//			 batters[2] = 1, 1, 0, 4, 3;
+//			 batters[3] = 1, 1, 2, 2, 4 ;
+//			 batters[4] = 0, 4, 1, 1, 2 ;
+			 
 			double sum = 0.000; // sets sum to double showing three places past
 								// decimal
 			for (int i = 0; i < times.length; i++) {// initiate for loop
-				sum += base[i];
+				sum += batters[i];
 				/*
 				 * tried a bunch of different variables in the following line in
 				 * an attempt to see which variable would get the output to
 				 * increment;
 				 */
-				System.out.println("Result for at-bat " + (i + 1) + ":  " + base[i]);
+				//System.out.println("For player:  " + num);
+				System.out.println("Result for at-bat " + (i + 1) + ":  " + batters[i]);
 			} // end for loop
 			System.out.println();
 			/*
@@ -69,7 +77,7 @@ public class BaseballAverages {
 			 * of instances where batter makes a base, currently dividing by
 			 * total number of times at bat.
 			 */
-			System.out.println("Batting average is:  " + (times.length / sum));//need to create method to calculate hits, not at bats
+			System.out.println("Batting average is:  " + (times.length / sum));//need to divide hits, by sum
 			System.out.println("Slugging Percent:  " + (sum / times.length));
 			System.out.println();
 
